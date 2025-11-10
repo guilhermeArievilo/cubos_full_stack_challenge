73b5ff8f14bbf35de8b84e0ae41179eb574d66a0
Guilherme Oliveira <guilherme_arievilo@outlook.com>
2025-11-10 16:22:49 -0300

    upd: add pagination

--- frontend/components/custom-pagination.tsx
+++ frontend/components/custom-pagination.tsx
@@ -1,3 +1,4 @@
+'use client'
 import { ChevronLeftIcon, ChevronRightIcon } from "lucide-react"
 
 import { usePagination } from "@/hooks/use-pagination"
@@ -10,6 +11,8 @@ import {
   PaginationItem,
   PaginationLink,
 } from "@/components/ui/pagination"
+
+import { useRouter, useSearchParams } from "next/navigation";
 
 type PaginationProps = {
   currentPage: number
@@ -28,6 +31,15 @@ export default function CustomPagination({
     paginationItemsToDisplay,
   })
 
+  const router = useRouter();
+  const searchParams = useSearchParams();
+
+  function handlePageChange(newPage: number) {
+    const params = new URLSearchParams(searchParams.toString())
+    params.set("page", newPage.toString())
+    router.push(`?${params.toString()}`)
+  }
+
   return (
     <Pagination>
       <PaginationContent className="inline-flex gap-4 -space-x-px rounded-md shadow-xs rtl:space-x-reverse">
@@ -39,10 +51,10 @@ export default function CustomPagination({
               }),
               "rounded-none shadow-none focus-visible:z-10 aria-disabled:pointer-events-none [&[aria-disabled]>svg]:opacity-50 px-6! border-none"
             )}
-            href={currentPage === 1 ? undefined : `#/page/${currentPage - 1}`}
             aria-label="Ir para página anterior"
             aria-disabled={currentPage === 1 ? true : undefined}
             role={currentPage === 1 ? "link" : undefined}
+            onClick={() => handlePageChange(Math.max(currentPage - 1, 1))}
           >
             <ChevronLeftIcon size={16} aria-hidden="true" />
           </PaginationLink>
@@ -56,7 +68,7 @@ export default function CustomPagination({
         )}
 
         {/* Page number links */}
-        {pages.map((page) => (
+        {pages.map((page, index) => (
           <PaginationItem key={page}>
             <PaginationLink
               className={cn(
@@ -66,7 +78,7 @@ export default function CustomPagination({
                 "rounded-none shadow-none focus-visible:z-10 border-none",
                 page === currentPage && "bg-accent"
               )}
-              href={`#/page/${page}`}
+              onClick={() => handlePageChange(index + 1)}
               isActive={page === currentPage}
             >
               {page}
@@ -97,10 +109,8 @@ export default function CustomPagination({
               }),
               "rounded-none shadow-none focus-visible:z-10 aria-disabled:pointer-events-none [&[aria-disabled]>svg]:opacity-50 px-6! border-none"
             )}
-            href={
-              currentPage === totalPages
-                ? undefined
-                : `#/page/${currentPage + 1}`
+            onClick={() =>
+              handlePageChange(Math.min(currentPage + 1, totalPages))
             }
             aria-label="Go to next page"
             aria-disabled={currentPage === totalPages ? true : undefined}
